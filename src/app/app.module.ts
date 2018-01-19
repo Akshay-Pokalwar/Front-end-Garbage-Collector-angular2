1dc81d924bb282f5486e6e8bcf2a761efd89473a
Akshay Pokalwar <akshaypklwr@gmail.com>
2018-01-19 20:41:17 +0530

    login page without bootstrap or css implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { LoginComponent } from '../pages/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms'
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Route} from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, Response } from '@angular/http';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public UserName:String;
+  public Password:String;
+
+  constructor(private http:HttpClient) { }
+
+
+  ngOnInit() {
+    // this.http.get("localhost:8080/user/namepassword/sanyam/test1").subscribe(data=>{
+    //   console.log(data);
+    // });
+  }
+
+  login()
+  {
+    console.log("UserName="+this.UserName+" Password="+this.Password );
+    this.http.get("http://localhost:8080/user/namepassword/"+this.UserName+"/"+this.Password).subscribe(
+      data=>console.log(data)
+  );
+}
+}
